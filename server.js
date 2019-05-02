132f99ca57d5f1571fd66e929ff3e35ae19922b2
Michael Sarmiento <misarmiento@csumb.edu>
2019-05-02 01:59:00 -0700

    move mongodb

--- server.js
+++ server.js
@@ -14,26 +14,6 @@ var express = require('express'),
 var app = express(),
   s3 = new aws.S3();
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/freshist", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // // Initialize the app.
-  // var server = app.listen(process.env.PORT || 8080, function () {
-  //   var port = server.address().port;
-  //   console.log("App now running on port", port);
-  // });
-});
 
 var upload = multer({
     storage: multerS3({
@@ -69,3 +49,25 @@ routes(app); //register the route
 app.listen(port);
 
 console.log('Freshist Engaged!' + port);
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/freshist", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // // Initialize the app.
+  // var server = app.listen(process.env.PORT || 8080, function () {
+  //   var port = server.address().port;
+  //   console.log("App now running on port", port);
+  // });
+});
+
